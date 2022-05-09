578a9bfdf85b83ffde14c53acfba9857e2f5b17c
alan8r-uwt <alan8r@uw.edu>
2022-05-08 20:43:50 -0700

    2nd attempt to get this working. cleaned up existing code and added Profile class and javadoc comments

--- Project360/src/main/MainWindow.java
+++ Project360/src/main/MainWindow.java
@@ -6,41 +6,63 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-
+/**
+ * 
+ * @author Alan Thompson
+ * 
+ * MainWindow.java
+ * 
+ * A class to handle creation of the main window frame and its associated components.
+ *
+ */
 
 public class MainWindow extends JFrame{
 	
-	// public static final String APP_VERSION = "v0.1"; <-unused
-	
-	private String user = "unregistered";
-	
-	JButton bAbout;
-	
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Class members
+	 */
+	private JButton bAbout;
+	
+	/**
+	 * MainWindow constructor
+	 * @param width
+	 * @param height
+	 */
 	public MainWindow(int width, int height) {
-		
+		initWindow(width, height);
+	}
+	
+	/**
+	 * Helper method to initialize the window and make it visible.
+	 * @param width
+	 * @param height
+	 */
+	public void initWindow(int width, int height) {
 		setTitle("TeamONE FileSorter");
 		
-		bAbout = new JButton("About");
-		bAbout.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				new AboutWindow(320, 240);
-			}
-		});
-		
-		bAbout.setBounds(10, 10, 100, 50);
-		add(bAbout);
+		createAboutButton();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(width, height);
 		setLayout(null);
 		setLocationRelativeTo(null);
-		setVisible(true);	
+		setVisible(true);
 	}
 	
-	public String getUser() {
-		return user;
+	/**
+	 * Helper method to initialize the About button and handle it's click event
+	 */
+	public void createAboutButton() {
+		bAbout = new JButton("About");
+		bAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new AboutWindow(Main.DEFAULT_WIDTH/2, (Main.DEFAULT_HEIGHT/2));
+			}
+		});
+		bAbout.setBounds(10, 10, 100, 50);
+		add(bAbout);
 	}
 }
 
